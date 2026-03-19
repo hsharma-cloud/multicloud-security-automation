cb90d09eee56bd6b67b8bfb5e8f18d16001013da
Hari Sharma <your-github-email@example.com>
2026-03-19 15:14:09 -0400

    Add subnet to VNet via CI/CD

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,11 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = data.azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "corp-dev-subnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
