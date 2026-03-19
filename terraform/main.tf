04f298ec3e5debd44d524be26f191fac5d771244
Hari Sharma <your-github-email@example.com>
2026-03-19 15:24:27 -0400

    Create subnet using existing VNet

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,23 +2,21 @@ provider "azurerm" {
   features {}
 }
 
-# Use existing Resource Group
+# Existing Resource Group
 data "azurerm_resource_group" "rg" {
   name = "corp-dev-rg"
 }
 
-# Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
+# Existing Virtual Network
+data "azurerm_virtual_network" "vnet" {
   name                = "corp-dev-vnet"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+  resource_group_name = "corp-dev-rg"
 }
 
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "corp-dev-subnet"
   resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
