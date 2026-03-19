4ed369a465ee6c12e3ecb09f6e0feff8eb86ca71
Hari Sharma <your-github-email@example.com>
2026-03-19 13:30:21 -0400

    Final fix: reuse subnet, stable AKS pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,33 @@ provider "azurerm" {
   features {}
 }
 
+# -------------------------------
 # EXISTING RESOURCE GROUP
+# -------------------------------
 data "azurerm_resource_group" "rg" {
   name = "corp-dev-rg"
 }
 
-# EXISTING VNET
+# -------------------------------
+# EXISTING VIRTUAL NETWORK
+# -------------------------------
 data "azurerm_virtual_network" "vnet" {
   name                = "corp-dev-vnet"
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-# CREATE SUBNET (safe)
-resource "azurerm_subnet" "subnet" {
+# -------------------------------
+# EXISTING SUBNET
+# -------------------------------
+data "azurerm_subnet" "subnet" {
   name                 = "corp-dev-subnet"
-  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = data.azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  resource_group_name  = data.azurerm_resource_group.rg.name
 }
 
-# AKS
+# -------------------------------
+# AKS CLUSTER (ONLY RESOURCE CREATED)
+# -------------------------------
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "corp-dev-aks"
   location            = data.azurerm_resource_group.rg.location
@@ -31,8 +38,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name           = "nodepool1"
     node_count     = 1
-    vm_size        = "Standard_DC2s_v3"
-    vnet_subnet_id = azurerm_subnet.subnet.id
+    vm_size        = "Standard_DC2s_v3"   # allowed in your subscription
+    vnet_subnet_id = data.azurerm_subnet.subnet.id
   }
 
   identity {
