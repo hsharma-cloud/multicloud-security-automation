46227bdb8cde8772dc8698aae4d3b400f3ec0c45
Hari Sharma <your-github-email@example.com>
2026-03-19 13:11:26 -0400

    Fix RG conflict and clean AKS config

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,65 +2,45 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "corp-dev-rg"
-  location = "East US"
+# Use existing Resource Group (DO NOT recreate)
+data "azurerm_resource_group" "rg" {
+  name = "corp-dev-rg"
 }
 
 # Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "corp-dev-vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-# Bastion Subnet (required name)
-resource "azurerm_subnet" "bastion" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+# Subnet for AKS
+resource "azurerm_subnet" "subnet" {
+  name                 = "corp-dev-subnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Public IP for Bastion
-resource "azurerm_public_ip" "bastion_ip" {
-  name                = "corp-dev-bastion-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-# Bastion Host
-resource "azurerm_bastion_host" "bastion" {
-  name                = "corp-dev-bastion"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.bastion.id
-    public_ip_address_id = azurerm_public_ip.bastion_ip.id
-  }
-}
-
+# AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "corp-dev-aks"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   dns_prefix          = "corpdevaks"
 
   default_node_pool {
-    name       = "nodepool"
+    name       = "nodepool1"
     node_count = 1
-    vm_size    = "Standard_DC2s_v3"
+    vm_size    = "Standard_DC2s_v3"   # ✅ works in your subscription
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  role_based_access_control_enabled = true
+  network_profile {
+    network_plugin = "azure"
+  }
 }
