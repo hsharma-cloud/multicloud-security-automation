57b14985ed97a2d967ba2312eb2ae1515d5589ff
Hari Sharma <your-github-email@example.com>
2026-03-19 14:45:10 -0400

    Enable Defender for Cloud (VM plan)

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,7 @@ resource "azurerm_resource_group" "rg" {
   name     = "corp-dev-rg"
   location = "eastus"
 }
+resource "azurerm_security_center_subscription_pricing" "defender" {
+  tier          = "Standard"
+  resource_type = "VirtualMachines"
+}
