dc1bf036c26db2a329921c6570a710d3d123d3f6
Hari Sharma <your-github-email@example.com>
2026-03-19 15:03:59 -0400

    Add VNet via CI/CD

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "corp-dev-rg"
-  location = "eastus"
+# Use existing Resource Group
+data "azurerm_resource_group" "rg" {
+  name = "corp-dev-rg"
 }
-resource "azurerm_security_center_subscription_pricing" "defender" {
-  tier          = "Standard"
-  resource_type = "VirtualMachines"
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "corp-dev-vnet"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
